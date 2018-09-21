e51046b7d269fb76579fadb577a4617e653c37ae
mnitaditya2006 <mnit.aditya@gmail.com>
2018-09-22 01:02:46 +0530

    cc

--- datastructures/src/main/java/om/aditya/datastructures/recursion/BinarySequenceWithoutConsicutiveZero.java
+++ datastructures/src/main/java/om/aditya/datastructures/recursion/BinarySequenceWithoutConsicutiveZero.java
@@ -2,6 +2,20 @@ package om.aditya.datastructures.recursion;
 
 public class BinarySequenceWithoutConsicutiveZero {
 
-    
+    public  static int numBinarySeq(int n) {
+
+        if (n==1) return 2 ;
+         else {
+            if (n==2) return 3;
+            else return numBinarySeq(n-1) + numBinarySeq(n-2);
+        }
+
+    }
+
+    public static void main(String args[]) {
+
+        System.out.println("total binary sequences for 3 are: " +numBinarySeq(3)) ;
+
+    }
 
 }
